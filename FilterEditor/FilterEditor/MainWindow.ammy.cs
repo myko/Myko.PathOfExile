aeaa335704a0bac97a93069e894697cc3c71c154
Christer Nyberg <myko82@gmail.com>
2018-06-05 12:34:44 +0200

    Initial commit Installed Fody and Ammy nuget packages.

---
+++ FilterEditor/FilterEditor/MainWindow.ammy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myko.PathOfExile.FilterEditor
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
